21f499a45b9a69a7b220ca1b2acddcd0ba121666
Piyush Dhall <dhall.piyush@gmail.com>
2023-03-17 01:30:51 +0530

    added habitable planets lookup api and wait the server till it laods

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,19 @@
 const http = require('http')
 const app = require('./app')
 
+const { loadPlanetsData } = require('./models/planets.model')
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}...`)
-})
+async function startServer() {
+    await loadPlanetsData()
+    console.log('Planets Data Loaded!')
+
+    server.listen(PORT, () => {
+        console.log(`Server started on PORT ${PORT}...`)
+    })
+}
+
+startServer();
